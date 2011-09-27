c34a22e00b1ba382c85a8f39bfec1bfeb25ae897
Kevin Rutherford <kevin@rutherford-software.com>
2011-09-27 20:57:19 +0100

    First spec passing

---
+++ spec/kata_spec.rb
@@ -0,0 +1,10 @@
+describe 'Ordered Jobs' do
+  context 'empty string' do
+    it 'returns an empty string' do
+      input = ''
+      output = ''
+      output.should == input
+    end
+  end
+end
+
