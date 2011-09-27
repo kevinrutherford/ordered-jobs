2d78bbd59b5cf8f6e3b5ede1114ed5532c6146bc
Kevin Rutherford <kevin@rutherford-software.com>
2011-09-27 21:06:14 +0100

    Step 2 green

--- spec/kata_spec.rb
+++ spec/kata_spec.rb
@@ -2,8 +2,16 @@ describe 'Ordered Jobs' do
   context 'empty string' do
     it 'returns an empty string' do
       input = ''
-      output = ''
-      output.should == input
+      output = input
+      output.should == ''
+    end
+  end
+
+  context 'single job' do
+    it 'returns the single job' do
+      input = 'a =>'
+      output = 'a'
+      output.should == 'a'
     end
   end
 end
