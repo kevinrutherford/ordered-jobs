6f43586c50fc20099591f78c168983e68d88420e
Kevin Rutherford <kevin@rutherford-software.com>
2011-09-30 17:35:32 +0100

    Clarified context in the instruction parser specs

--- spec/instruction_spec.rb
+++ spec/instruction_spec.rb
@@ -3,9 +3,11 @@ require './lib/job'
 
 describe Instruction do
   describe '.parse' do
-    it 'creates a new Job object' do
-      Job.should_receive(:new).with('a').exactly(:once)
-      Instruction.parse('a =>')
+    context 'with no dependency' do
+      it 'creates a new Job object' do
+        Job.should_receive(:new).with('a').exactly(:once)
+        Instruction.parse('a =>')
+      end
     end
   end
 end
