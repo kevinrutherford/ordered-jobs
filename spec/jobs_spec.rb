45126f04a08a581027a62479631055b1e090e5c1
Kevin Rutherford <kevin@rutherford-software.com>
2011-09-30 20:03:47 +0100

    Extracted test setups for job sorting

--- spec/jobs_spec.rb
+++ spec/jobs_spec.rb
@@ -3,22 +3,25 @@ describe Job do
     subject { Job.new('h') }
 
     context 'given identical jobs' do
+      let(:other) { Job.new('h') }
+
       it 'compares equal' do
-        other = Job.new('h')
         subject.<=>(other).should == 0
       end
     end
 
     context 'given a job later in the alphabet' do
+      let(:other) { Job.new('s') }
+
       it 'will sort the jobs alphebatically' do
-        other = Job.new('s')
         subject.<=>(other).should < 0
       end
     end
 
     context 'given a job earlier in the alphabet' do
+      let(:other) { Job.new('c') }
+
       it 'will sort the jobs alphebatically' do
-        other = Job.new('c')
         subject.<=>(other).should > 0
       end
     end
