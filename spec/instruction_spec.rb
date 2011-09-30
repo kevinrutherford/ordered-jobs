1a93cb60be135aebc7b604fbbd1b0984fb57d182
Kevin Rutherford <kevin@rutherford-software.com>
2011-09-30 12:53:47 +0100

    Clarified the test for instruction parsing

--- spec/instruction_spec.rb
+++ spec/instruction_spec.rb
@@ -3,7 +3,8 @@ require './lib/instruction'
 describe Instruction do
   describe '.parse' do
     it 'creates a new Instruction object' do
-      Instruction.parse('a =>').should_not be_nil
+      Instruction.should_receive(:new).exactly(:once)
+      Instruction.parse('a =>')
     end
   end
 end
