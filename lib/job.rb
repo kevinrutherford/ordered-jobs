0195a4747aa0c8e35c60371b614948a8be423818
Kevin Rutherford <kevin@rutherford-software.com>
2011-09-30 20:48:56 +0100

    Job dependency now affects the sorting predicate

--- lib/job.rb
+++ lib/job.rb
@@ -6,11 +6,21 @@ class Job
     @name = name
   end
 
+  def depends_on(predecessor)
+    @predecessor = predecessor
+  end
+
+  def depends_on?(other)
+    @predecessor && @predecessor.name == other.name
+  end
+
   def add_to(schedule)
     schedule.add(@name)
   end
 
   def <=>(other)
+    return 1 if self.depends_on?(other)
+    return -1 if other.depends_on?(self)
     @name <=> other.name
   end
 
--- spec/jobs_spec.rb
+++ spec/jobs_spec.rb
@@ -1,7 +1,34 @@
 describe Job do
-  describe '#<=>' do
-    subject { Job.new('h') }
+  let(:other) { Job.new('s') }
+  subject { Job.new('h') }
 
+  describe '#depends_on?' do
+    context 'with an unrelated job' do
+      it 'does not depend on me' do
+        other.depends_on?(subject).should be_false
+      end
+
+      it 'I do not depend on it' do
+        subject.depends_on?(other).should be_false
+      end
+    end
+
+    context 'given a job that depends on me' do
+      before do
+        other.depends_on(subject)
+      end
+
+      it 'depend on me' do
+        other.depends_on?(subject).should be_true
+      end
+
+      it 'I do not depend on it' do
+        subject.depends_on?(other).should be_false
+      end
+    end
+  end
+
+  describe '#<=>' do
     context 'given identical jobs' do
       let(:other) { Job.new('h') }
 
@@ -16,6 +43,16 @@ describe Job do
       it 'will sort the jobs alphebatically' do
         subject.<=>(other).should < 0
       end
+
+      context 'when I depend on it' do
+        before do
+          subject.depends_on(other)
+        end
+
+        it 'places other before me' do
+          subject.<=>(other).should > 0
+        end
+      end
     end
 
     context 'given a job earlier in the alphabet' do
@@ -23,6 +60,16 @@ describe Job do
 
       it 'will sort the jobs alphebatically' do
         subject.<=>(other).should > 0
+      end
+
+      context 'when it depends on me' do
+        before do
+          other.depends_on(subject)
+        end
+
+        it 'places me first' do
+          subject.<=>(other).should < 0
+        end
       end
     end
 
