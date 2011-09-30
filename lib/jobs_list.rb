6c07aa8f7685573b8a8a912aff6a1cb73f661d44
Kevin Rutherford <kevin@rutherford-software.com>
2011-09-30 17:18:56 +0100

    Improved names in the JobsList constructor

--- lib/jobs_list.rb
+++ lib/jobs_list.rb
@@ -1,11 +1,12 @@
 class JobsList
 
   def initialize(spec)
-    @instructions = spec.split("\n").map {|instr| Instruction.parse(instr) }
+    instructions = spec.split("\n")
+    @jobs = instructions.map {|instr| Instruction.parse(instr) }
   end
 
   def add_to(schedule)
-    @instructions.each {|i| i.add_to(schedule) }
+    @jobs.each {|i| i.add_to(schedule) }
   end
 
 end
